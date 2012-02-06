4079782454bb1244dd4747ccafeb57c8e579b353
Zachary D. Shaw <zdsbs@yahoo.com>
2012-02-06 17:45:43 -0500

    resetting workspace

--- src/net/m14m/katas/messaging/Action.java
+++
@@ -1,7 +0,0 @@
-package net.m14m.katas.messaging;
-
-public interface Action {
-
-	void apply(String address, String body);
-
-}
--- src/net/m14m/katas/messaging/Main.java
+++ src/net/m14m/katas/messaging/Main.java
@@ -15,25 +15,6 @@ public class Main {
 	}
 
 	public static void main(String... args) {
-		SendEmail sendEmail = new SendEmail(network);
-		BadEmailAddress badEmailAddress = new BadEmailAddress(console);
-		MissingEmailBody missingEmailBody = new MissingEmailBody(console);
-		ForeachEmailAddress foreachEmailAddress = new ForeachEmailAddress();
-		missingEmailBody.otherwise(badEmailAddress).otherwise(noop());
-
-		foreachEmailAddress.run(missingEmailBody).noErros(sendEmail);
-
-		Runner runner = new Runner();
-		runner.start(foreachEmailAddress).using(args);
 	}
 
-	private static Action noop() {
-		return new Action() {
-
-			@Override
-			public void apply(String address, String body) {
-
-			}
-		};
-	}
 }
